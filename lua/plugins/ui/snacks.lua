5ff8b3afee53e72d19dbd805df3d033e74a78fbf
Riverxia <riverdalecoder@outlook.com>
2026-01-04 11:52:59 -0500

    upd

--- lua/plugins/ui/snacks.lua
+++ lua/plugins/ui/snacks.lua
@@ -7,7 +7,7 @@ return {
 		bigfile = {enabled=true},
 		dashboard = {enabled=true,
 		preset = {
-			header = [[	                                                                   
+			header = [[	                                                                    
        ████ ██████           █████      ██                     
       ███████████             █████                             
       █████████ ███████████████████ ███   ███████████   
