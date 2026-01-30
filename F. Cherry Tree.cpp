cb5cca525e7230b68cc55abdfaa160b6a9c6b8e1
Riverxia <riverdalecoder@outlook.com>
2026-01-30 15:45:34 -0500

    latest

---
+++ F. Cherry Tree.cpp
@@ -0,0 +1,126 @@
+// Problem: F. Cherry Tree
+// Judge: Codeforces
+// URL: https://codeforces.com/contest/2184/problem/F
+// Memory Limit: 256 MB
+// Time Limit: 2000 ms
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+static const uint64_t CHRONO_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+static const uint64_t PIDRNDP = (uint64_t)(getpid())*0xbffbffbffULL;
+static mt19937_64 PIDRNG(PIDRNDP);
+static const uint64_t PIDRND = PIDRNG();
+static const uint64_t FIXED_RANDOM = CHRONO_RANDOM ^ PIDRND;
+struct CHASH {
+    template <typename T> size_t operator()(const T& x) const {
+        return hash<T>{}(x) ^ FIXED_RANDOM;
+    }
+    template <typename T1, typename T2> size_t operator()(const pair<T1, T2>& x) const {
+        return (*this)(x.first) ^ ((*this)(x.second) + 0x9e3779b9 + (x.first << 6) + (x.first >> 2));
+    }
+};
+template<class T, class U> istream& operator>>(istream& i, pair<T, U>& p) { return i >> p.first >> p.second; }
+template<class T, class U> ostream& operator<<(ostream& o, const pair<T, U>& p) { return o << p.first << " " << p.second; }
+template<class T> istream& operator>>(istream& i, vector<T>& v) { for(auto& x : v) i >> x; return i; }
+template<class T> ostream& operator<<(ostream& o, const vector<T>& v) { for(int i=0; i<v.size(); ++i) o << v[i] << (i==v.size()-1?"":" "); return o; }
+#define m1(x) template<class T, class... U> void x(T&& a, U&&... b)
+#define m2(x) (int[]){(x std::forward<U>(b),0)...}
+template<typename T1,typename T2> using hashmap=unordered_map<T1,T2,CHASH>;
+template<typename TM> using matrix=vector<vector<TM>>;
+using graph=matrix<int>;
+template<typename TM> using tensor=vector<matrix<TM>>;
+template<typename TM> using hypermatrix=vector<tensor<TM>>;
+template<typename TM, TM Val = TM(), typename... Args> auto make(size_t first, Args... args){
+    if constexpr(sizeof...(args) == 0){
+        return vector<TM>(first, Val);
+    } else {
+        return vector<decltype(make<TM, Val>(args...))>(first, make<TM, Val>(args...));
+    }
+}
+#define all(x) (x).begin(),(x).end()
+#define forn(i,n) for(int i=0;i<(n);++i)
+#define f0rn(v,s,e) for(int v=(s);v>(e);--v)
+#define fOrn(v,s,e) for(int v=(s);v<(e);++v)
+#define INTERACTIVE false
+#ifndef LOCAL_JUDGE
+#define FILEMODE false
+#define FILENAME "pname"
+#endif
+#if INTERACTIVE
+m1(out) { cout << std::forward<T>(a);  m2(cout << " " <<); cout << endl; }//softmod for interactive
+m1(debug) { cerr << std::forward<T>(a);  m2(cerr << " " <<); cerr << "\n"; }
+m1(in) { cin >> std::forward<T>(a); m2(cin >>); }
+#else
+m1(out) { cout << std::forward<T>(a);  m2(cout << " " <<); cout << "\n"; }//softmod for interactive
+m1(debug) { cerr << std::forward<T>(a);  m2(cerr << " " <<); cerr << "\n"; }
+m1(in) { cin >> std::forward<T>(a); m2(cin >>); }
+#endif
+#define MULTITEST true
+#define pb push_back
+void solve(){
+    int n;
+    in(n);
+    
+    matrix<int> adj(n + 1);
+    forn(i, n - 1) {
+        int u, v;
+        in(u, v);
+        adj[u].pb(v);
+        adj[v].pb(u);
+    }
+    
+    matrix<int> dp = make<int>(n + 1, 3);
+    
+    function<void(int, int)> dfs = [&](int u, int p) -> void {
+        bool is_leaf = true;
+        vector<int> current_sums = {1, 0, 0};
+        
+        for (int v : adj[u]) {
+            if (v == p) continue;
+            is_leaf = false;
+            
+            dfs(v, u);
+            
+            vector<int> next_sums = {0, 0, 0};
+            forn(i, 3) {
+                if (current_sums[i]) {
+                    forn(k, 3) {
+                        if (dp[v][k]) {
+                            next_sums[(i + k) % 3] = 1;
+                        }
+                    }
+                }
+            }
+            current_sums = next_sums;
+        }
+        
+        if (is_leaf) {
+            dp[u][0] = 0;
+            dp[u][1] = 1;
+            dp[u][2] = 0;
+        } else {
+            forn(i, 3) dp[u][i] = current_sums[i];
+            dp[u][1] = 1;
+        }
+    };
+    
+    dfs(1, -1);
+    
+    if (dp[1][0]) {
+        out("YES");
+    } else {
+        out("NO");
+    }
+}
+int main(){
+    if(!INTERACTIVE)cin.tie(0)->sync_with_stdio(0);
+    #ifndef LOCAL_JUDGE
+    #if FILEMODE
+    freopen(FILENAME".in","r",stdin);
+    freopen(FILENAME".out","w",stdout);
+    #endif
+    #endif
+    int t=1;
+    if (MULTITEST) cin>>t;
+    forn(i,t)solve();
+}
--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -8,6 +8,7 @@ local servers = {
   "cssls",
   "yamlls",
   "eslint",
+  "eslint_d",
   "pyright",
   "rust_analyzer",
   "ast_grep",
